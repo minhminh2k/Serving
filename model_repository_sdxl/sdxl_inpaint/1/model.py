2c06a54f98918c875f1b1c8a601efb03c2520084
minhminh2k <minhqd9112003@gmail.com>
2025-06-02 16:20:47 +0000

    TODO: SDXL Inpaint IDM pipeline

--- model_repository_sdxl/sdxl_inpaint/1/model.py
+++ model_repository_sdxl/sdxl_inpaint/1/model.py
@@ -1,25 +1,6 @@
-#  Copyright 2022, Lefebvre Dalloz Services
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
-"""
-This module is copy-pasted in generated Triton configuration folder to perform inference.
-"""
-
 import inspect
 import logging
 
-# noinspection DuplicatedCode
 from pathlib import Path
 from typing import Dict, List, Union
 
@@ -35,6 +16,8 @@ from diffusers.schedulers import (
     LMSDiscreteScheduler,
     PNDMScheduler,
 )
+from diffusers import StableDiffusionXLInpaintPipeline
+from optimum.onnxruntime import ORTStableDiffusionXLInpaintPipeline
 
 
 try:
